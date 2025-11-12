757d4bc209b1912913aac1adbec695d57daa6d4b
PGalmeida <pedro8tp@gmail.com>
2025-11-12 07:41:54 -0300

    feat: criar criptografia e login do sistema

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import catchAsyncErrors from "../middleware/catchAsyncErrors.js";
 import User from "../models/user.js";
+import ErrorHandler from "../utils/errorHandle.js";
 
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -10,8 +11,35 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
     password,
   });
 
+  const token = user.getJwtToken();
+
   res.status(201).json({
-    success: true,
-    user,
+    token,
+  });
+});
+
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Por favor, insira email e senha", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Por favor, informe o usuario", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Senha incorreta", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(201).json({
+    token,
   });
 });
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -27,5 +29,23 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
